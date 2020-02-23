d48fae227ed180b0bd066e4d01ee922784f8f980
jnx86bcn <jnx86bcn@gmail.com>
2020-02-23 09:24:50 +0100

    update new files client

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+
+    entry: './client/app.js',
+
+    output: {
+        path: path.join(__dirname, 'server/public'),
+        filename: 'js/bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'client/index.html',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'branding/styles/bundle.css'
+        })
+    ],
+
+    devtool: 'source-map'
+
+};
