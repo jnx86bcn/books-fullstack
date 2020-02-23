ef4a58370eb04e35ab87322f3a0c6b4cdbc855f5
jnx86bcn <jnx86bcn@gmail.com>
2020-02-23 09:17:30 +0100

    upload client folder

---
+++ client/UI.js
@@ -0,0 +1,69 @@
+import BookService from './services/BookService';
+import {format} from 'timeago.js';
+
+const bookSVC = new BookService();
+
+class UI {
+
+    async renderBooks() {
+        const books = await bookSVC.getBooks();
+        const booksCardContainer = document.getElementById('books-cards');
+        booksCardContainer.innerHTML = '';
+        books.forEach(book => {
+            const div = document.createElement('div');
+            div.className = "";
+            div.innerHTML = `
+            <div class="card m-2">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="${book.coverPath}" alt="" class="img-fluid"/>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-block px-2">
+                            <h4 class="card-title">${book.title}</h4>
+                            <p class="card-text">${book.author}</p>
+                            <a href=# class="btn btn-danger delete" _id="${book._id}">X</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    ${format(books.created)}
+                </div>
+            </div>
+            `;
+            booksCardContainer.appendChild(div);
+        });
+    }
+
+    async addNewBook(book) {
+        await bookSVC.postBook(book);
+        await this.renderBooks();
+        this.clearBookForm();
+    }
+
+    clearBookForm() {
+        document.getElementById('book-form').reset();
+    }
+
+    renderMessage(message,colorMessage,secondsToRemove) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${colorMessage} message`;
+        div.appendChild(document.createTextNode(message));
+
+        const container = document.querySelector('.col-md-4');
+        const bookForm = document.querySelector('#book-form');
+
+        container.insertBefore(div,bookForm);
+
+        setTimeout(() => {
+            document.querySelector('.message').remove();
+        }, secondsToRemove*1000);
+    }
+
+    async deleteBook(bookId) {
+        await bookSVC.deleteBook(bookId);
+        await this.renderBooks();
+    }
+
+}
+export default UI
