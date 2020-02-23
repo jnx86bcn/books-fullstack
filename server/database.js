e1eee5cd28a79665e924279503caab671aff54b7
jnx86bcn <jnx86bcn@gmail.com>
2020-02-23 09:16:45 +0100

    commit

--- server/database.js
+++ server/database.js
@@ -1,23 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const devMode = process.env.NODE_ENV !== 'production';
-
-// connect();
-
-// //connect db
-// async function connect() {
-//     try{
-//         await mongoose.connect(process.env.MONGODB_URI,{
-//             useNewUrlParser:true,    
-//             useUnifiedTopology: true
-//         });
-//         console.log('DB is connected');
-//     }
-//     catch(error){
-//         console.log(error);
-//     }
-// }
-
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI, {
